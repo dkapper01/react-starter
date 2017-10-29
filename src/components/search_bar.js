89b51e29c89d5018aea9dd2cc5a8770a48ca0b3d
Daniel Kapper <dkapper01@gmail.com>
2017-10-29 11:56:35 -0400

    changed state term

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  construtor(props) {
+  constructor(props) {
     super(props);
 
     this.state = { term: '' };
   }
   render() {
-    return <input onChange={event => console.log(event.target.value)} />
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+      </div>
+    );
   }
-
-
 }
 
 
