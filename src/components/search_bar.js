982413261e79c5a44bf6ff11892ec20a42d292ed
Daniel Kapper <dkapper01@gmail.com>
2017-10-29 11:29:42 -0400

    added term in state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  construtor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
   render() {
     return <input onChange={event => console.log(event.target.value)} />
   }
 
-  // onInputChange(event) {
-  //   console.log(event.target.value);
-  // }
+
 }
 
 
