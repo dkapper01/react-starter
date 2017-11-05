946916ec24ccd515c07e53b90f3b049b8b1a6b60
Daniel Kapper <dkapper01@gmail.com>
2017-11-05 15:02:06 -0500

    add build

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyB7OaGTqQrLWsMCbk6OMom-301Y4Xxyc3g';
 
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
@@ -13,17 +14,25 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={console.log}
+          videos={this.state.videos} />
       </div>
     );
   }
